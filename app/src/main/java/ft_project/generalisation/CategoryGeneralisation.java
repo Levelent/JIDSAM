b3dad4898e9715cc6b12647f9815c0f10d5e6c7d
dmollett <44062144+dmollett@users.noreply.github.com>
2022-02-18 10:25:48 +0000

    Moved generalisation classes into seperate file

---
+++ app/src/main/java/ft_project/generalisation/CategoryGeneralisation.java
@@ -0,0 +1,25 @@
+package ft_project.generalisation;
+import ft_project.DGH;
+
+public class CategoryGeneralisation implements GeneralisationInterface{
+
+    private DGH dgh;
+    private String localRoot; //The name of the node the generalisation is locally rooted
+
+    CategoryGeneralisation(DGH dgh){
+        this.dgh = dgh;
+    }
+
+    public float infoLoss() {
+        return (dgh.countNodes(localRoot) -1)/(dgh.countNodes() - 1);
+    }
+    
+    public Boolean updateGeneralisation(String localRoot){
+        if(dgh.contains(localRoot)){
+            this.localRoot = localRoot;
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/ft_project/generalisation/GeneralisationInterface.java
@@ -0,0 +1,7 @@
+package ft_project.generalisation;
+
+public interface GeneralisationInterface {
+    public float infoLoss();
+    
+    // public Boolean updateGeneralisation(); // Can't be in the interface as they take different parameters
+}
