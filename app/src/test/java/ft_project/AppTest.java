a3b287557675f2a1ba772608039eafca4d473327
nkosi <nkosik11@gmail.com>
2022-02-25 02:08:55 +0000

    ...

--- app/src/test/java/ft_project/AppTest.java
+++ app/src/test/java/ft_project/AppTest.java
@@ -3,12 +3,9 @@
  */
 package ft_project;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+   
 }
