7fb94a56719039141e55ea4592ab40880c27aca4
nkosi <nkosik11@gmail.com>
2022-02-25 02:08:29 +0000

    file for testing

---
+++ app/src/main/java/ft_project/Scratch.java
@@ -0,0 +1,10 @@
+package ft_project;
+import java.io.*;
+import java.util.*;
+
+public class Scratch {
+    public static void main(String[] args){
+
+        DGHReader reader = new DGHReader("./app/src/main/resources/dgh");
+    }   
+}
