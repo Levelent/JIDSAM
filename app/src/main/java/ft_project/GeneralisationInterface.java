1696f854a13a5ff4a80304ef4056826f1feb688f
nkosi <nkosik11@gmail.com>
2022-02-28 11:03:08 +0000

    ...

--- app/src/main/java/ft_project/GeneralisationInterface.java
+++
@@ -1,8 +0,0 @@
-package ft_project.generalisation;
-
-public interface GeneralisationInterface {
-    public float infoLoss();
-
-    // public Boolean updateGeneralisation(); // Can't be in the interface as they
-    // take different parameters
-}
--- app/src/main/java/ft_project/Scratch.java
+++ app/src/main/java/ft_project/Scratch.java
@@ -5,6 +5,29 @@ import java.util.*;
 public class Scratch {
     public static void main(String[] args){
 
-        DGHReader reader = new DGHReader("./app/src/main/resources/dgh");
+       
+        // create data stream
+        Stream dataStream = new Stream("./app/src/main/resources/adult.csv");
+        Map<String, DGH> DGHs = new DGHReader("./app/src/main/resources/dgh").DGHs;
+        //System.out.println(DGHs);
+        Cluster c = new Cluster(dataStream.next(), DGHs);
+        c.add(dataStream.next());
+        c.add(dataStream.next());
+
+
+        Cluster b = null;
+        try{
+            b = (Cluster) c.clone();
+        }catch(CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+
+        c.add(dataStream.next());
+        c.add(dataStream.next());
+
+        System.out.println( c );
+        System.out.println( "\n\n");
+        System.out.println( b );
+
     }   
 }
