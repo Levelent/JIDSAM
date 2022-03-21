9018570d4266fbcb4a3608e5a329e24a5ab6121b
dmollett <44062144+dmollett@users.noreply.github.com>
2022-03-21 19:25:30 +0000

    tests

--- app/src/test/java/ft_project/CategoryGeneralisationTest.java
+++ app/src/test/java/ft_project/CategoryGeneralisationTest.java
@@ -3,12 +3,37 @@
  */
 package ft_project;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryGeneralisationTest {
+    DGH d;
+    CategoryGeneralisation g;
+
+    @BeforeEach
+    public void beforeEach() {
+        d = new DGH("location");
+        d.add("location", "west midlands");
+        d.add("west midlands", "coventry");
+        d.add("west midlands", "birmingham");
+        g = new CategoryGeneralisation(d, "coventry");
+    }
+
+    // TODO test info loss
+
+    // TODO test updateGeneralisation
+
     @Test
-    public void testTrue() {
-        assertTrue(true);
+    public void testToString() {
+        assertEquals("coventry", g.toString());
+    }
+
+    @Test
+    public void testClone() {
+        try {
+            assertNotEquals(g, g.clone());
+        } catch (CloneNotSupportedException e) {
+            fail("unexpected clone not supported exception");
+        }
     }
 }
