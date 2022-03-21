4b71ba0505bde3b60516076c21fd569e0112de44
dmollett <44062144+dmollett@users.noreply.github.com>
2022-03-21 19:08:11 +0000

    test DGHReader finished

--- app/src/test/java/ft_project/DGHReaderTest.java
+++ app/src/test/java/ft_project/DGHReaderTest.java
@@ -6,8 +6,34 @@ package ft_project;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 class DGHReaderTest {
-    // TODO constructor
+    @Test
+    public void testInStream() {
+        try {
+            // create temp file
+            File temp = File.createTempFile("temp", ".txt");
+            FileWriter writer = new FileWriter(temp);
+            writer.write(
+                    "$location" + System.lineSeparator() + "west midlands" + System.lineSeparator() + "    coventry"
+                            + System.lineSeparator() + "    birmingham");
+            writer.close();
+
+            // read DGH
+            DGHReader reader = new DGHReader(temp.getAbsolutePath());
+
+            // test dgh
+            DGH location = reader.DGHs.get("location");
+            assertNotNull(location);
+            assertEquals("west midlands", location.getRootValue());
+            assertNotNull(location.find("coventry"));
+        } catch (IOException e) {
+            fail("IO Exception not expected");
+        }
+    }
 
     @Test
     public void testIndentCount() {
