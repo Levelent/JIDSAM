2064680b604b418ef21c56de59fb69a13b07f85d
nkosi <nkosik11@gmail.com>
2022-03-22 21:59:32 +0000

    test cases

--- app/src/test/java/ft_project/DGHReaderTest.java
+++ app/src/test/java/ft_project/DGHReaderTest.java
@@ -3,12 +3,55 @@
  */
 package ft_project;
 
+import java.util.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DGHReaderTest {
+    static Map<String, DGH> DghMap;
+
     @Test
     public void testTrue() {
         assertTrue(true);
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        DGHReader DGHr = new DGHReader(".src/test/resources/dgh-indents");
+        DghMap = DGHr.DGHs;
+    }
+
+    @Test
+    public void testIndents() {
+        DGH indents = DghMap.get("dgh-indents");
+
+        assertTrue(indents.countNodes("1") == 1);
+        assertTrue(indents.countNodes("1.1") == 6);
+        assertTrue(indents.countNodes("1.2") == 1);
+        assertTrue(indents.countNodes("1.3") == 7);
+        assertTrue(indents.countNodes("1.4") == 1);
+
+    }
+
+    @Test
+    public void testNames() {
+        assertAll(
+                () -> {
+                    DGH dgh = DghMap.get("name-test1");
+                    assertTrue(dgh != null);
+                    assertTrue(dgh.countNodes() == 2);
+                },
+                () -> {
+                    DGH dgh = DghMap.get("name-test2");
+                    assertTrue(dgh != null);
+                    assertTrue(dgh.countNodes() == 4);
+                },
+                () -> {
+                    DGH dgh = DghMap.get("name-test3");
+                    assertTrue(dgh != null);
+                    assertTrue(dgh.countNodes() == 1);
+                });
     }
 }
