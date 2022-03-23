637ba8b72990d65e42ac918efd741816efc3b340
nkosi <nkosik11@gmail.com>
2022-03-23 19:18:51 +0000

    add xband variant

--- app/src/main/java/ft_project/App.java
+++ app/src/main/java/ft_project/App.java
@@ -8,16 +8,18 @@ public class App {
      */
     public static void main(String[] args) {
         // predefine thresholds/constants
-        int k = 5;
-        int delta = 10;
+        int k = 3;
+        int delta = 15;
         int beta = 2;
+        int omega = 100; // TODO refine this
+        int expirationBand = 5;// Gamma
 
         // l diversity thresholds/constants
         int l = 2;
         int a_s = 2;
 
         // create data stream
-        InStream dataStream = new InStream("./src/main/resources/adult-100.csv");
+        InStream dataStream = new InStream("./app/src/main/resources/adult-100.csv");
 
         // create data out stream
         OutStream outputStream = new OutStream("output.txt");
@@ -41,13 +43,15 @@ public class App {
                 // run FADS with l diversity (note beta is t_kc)
                 castle = new FADSL(dataStream, k, delta, beta, l, a_s);
                 break;
+            case "5":
+                castle = new XBAND(dataStream, k, delta, beta, omega, expirationBand);
             default:
                 // run normal castle
                 castle = new Castle(dataStream, k, delta, beta);
         }
 
         // set DGHs and output stream
-        castle.setDGHs(new DGHReader("./src/main/resources/dgh").DGHs);
+        castle.setDGHs(new DGHReader("./app/src/main/resources/dgh").DGHs);
 
         castle.setOutputStream(outputStream);
 
